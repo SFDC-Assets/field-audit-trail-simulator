f117d6bdb53485e56db0acf3d925882db47816d8
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:19:10 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,18 +1,6 @@
-# Create the demo org
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix field -o audit.trail 
-
-# Push the metadata into the new scratch org.
-sfdx force:source:push
-
-# Assign permission set
-sfdx force:user:permset:assign -n fat_simulator
-
-# Run anonymous apex to generate history for all existing contacts
-sfdx force:apex:execute -f scripts/apex/genContactHistory.apex
-
-# Set the default password.
-sfdx shane:user:password:set -g User -l User -p salesforce1
-
-# Open the org.
-sfdx force:org:open -p /lightning/o/Contact/list/
-
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p field -e audit.trail
+sf project deploy start
+sf org assign permset -n fat_simulator
+sf apex run -f scripts/apex/genContactHistory.apex
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org open -p /lightning/o/Contact/list/
